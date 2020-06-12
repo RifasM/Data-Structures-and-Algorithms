ad5a43c09e63a45059f6c67d66f2e0153646ab73
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-12 20:57:50 +0530

    FractionalKnapsack

--- Course 1/week3_greedy_algorithms/2_maximum_value_of_the_loot/FractionalKnapsack.java
+++ Course 1/week3_greedy_algorithms/2_maximum_value_of_the_loot/FractionalKnapsack.java
@@ -1,36 +1,47 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
+
+class knapsack{
+    Double cost;
+    int value, weight, index;
+
+    public knapsack(int index, int value, int weight){
+        this.index = index;
+        this.value = value;
+        this.weight = weight;
+        this.cost = (double)value / (double)weight;
+    }
+}
+
+class knapsackCompare implements Comparator<knapsack>{
+
+    @Override
+    public int compare(knapsack o1, knapsack o2) {
+        return o2.cost.compareTo(o1.cost);
+    }
+}
 
 public class FractionalKnapsack {
     private static double getOptimalValue(int capacity, int[] values, int[] weights) {
         double value = 0;
-        double[] cost = new double[values.length];
-        for(int i = 0; i < values.length ; i++)
-            cost[i] = (double) weights[i] / values[i];
-        for(int i = 0 ;i < cost.length-1 ; i++){
-            if(cost[i] < cost[i+1]){
-                double t = cost[i];
-                cost[i] = cost[i+1];
-                cost[i+1] = t;
-                int y = values[i];
-                values[i] = values[i+1];
-                values[i+1] = y;
-                y = weights[i];
-                weights[i] = weights[i+1];
-                weights[i+1] = y;
-            }
-        }
-        int i = 0;
-        while (capacity >= 0 && i < cost.length){
-            if((capacity - weights[i]) >= 0){
-                capacity -= weights[i];
-                value += values[i];
+        knapsack[] sack = new knapsack[values.length];
+        for(int i = 0 ; i < values.length ; i++)
+            sack[i] = new knapsack(i, values[i], weights[i]);
+        knapsackCompare compare = new knapsackCompare();
+        Arrays.sort(sack, compare);
+        for(knapsack a : sack){
+            int wt = a.weight;
+            int vl = a.value;
+            if(capacity - wt >= 0){
+                capacity -= wt;
+                value += vl;
             }
             else {
-                double fraction = (double) capacity / (double) weights[i];
-                value += fraction*values[i];
-                capacity -= fraction*weights[i];
+                double fraction = (double) capacity / (double) wt;
+                value += fraction*vl;
+                break;
             }
-            i++;
         }
         return value;
     }
