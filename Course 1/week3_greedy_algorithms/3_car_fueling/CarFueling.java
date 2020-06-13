dfe147b209ec7421943bcb01b4e027d6016ce10a
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-13 15:58:39 +0530

    Car Fueling

--- Course 1/week3_greedy_algorithms/3_car_fueling/CarFueling.java
+++ Course 1/week3_greedy_algorithms/3_car_fueling/CarFueling.java
@@ -6,22 +6,47 @@ public class CarFueling {
         int count = 0;
         if(stops.length <= 0 || (stops[stops.length-1] + tank < dist))
             return -1;
-        for(int i = 0 ; i<stops.length-1; i++){
+/*
+        for(int i = 0 ; i<stops.length-1; i++)
             if(stops[i+1] - stops[i] > tank)
-                return -1;
+                return -1;*/
+
+        int current = 0;
+        int nextPos = 0;
+        ArrayList<Integer> locations = new ArrayList<Integer>();
+        for(int stop : stops){
+            if(stop < current + tank)
+                continue;
+            else if(current + tank == stop) {
+                locations.add(stop);
+                current = stop;
+            }
+            else{
+                for(int i = stop; i > current; i--)
+                    if(Arrays.binarySearch(stops, i) > 0){
+                        current = i;
+                        count++;
+                        dist -= i;
+                        break;
+                    }
+            }
         }
+
+        if(locations.get(locations.size()-1) + tank >= dist)
+            return locations.size();
+        else
+            return -1;
+/*
         int cur = 0;
         int pos = cur + tank;
         while(dist>=0){
-            if(cur > dist)
-                break;
-            else if(Arrays.binarySearch(stops, pos) > 0) {
+            if(Arrays.binarySearch(stops, pos) > 0) {
                 cur = pos;
                 count++;
                 dist -= cur;
             }
             else{
-                for(int i = cur+tank-1; i > cur; i--)
+                for(int i = pos; i > cur; i--)
                     if(Arrays.binarySearch(stops, i) > 0){
                         cur = i;
                         count++;
@@ -30,8 +55,8 @@ public class CarFueling {
                     }
             }
             pos = cur + tank;
-        }
-        return count;
+        }*/
+        //return count;
     }
 
     public static void main(String[] args) {
