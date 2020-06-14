0b1120990a3c80f72100a8884bebcdc7e3530212
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-14 20:31:41 +0530

    Car Fueling

--- Course 1/week3_greedy_algorithms/3_car_fueling/CarFueling.java
+++ Course 1/week3_greedy_algorithms/3_car_fueling/CarFueling.java
@@ -2,28 +2,26 @@ import java.util.*;
 import java.io.*;
 
 public class CarFueling {
-    static int computeMinRefills(int dist, int tank, int[] stops) {
-        if(stops.length <= 0 || (stops[stops.length-1] + tank < dist))
-            return -1;
+    static int computeMinRefills(int dist, int tank, ArrayList<Integer> stops) {
 
-        int current = 0;
-        int nextPos;
-        ArrayList<Integer> locations = new ArrayList<>();
-        for(int i = 0 ; i<stops.length-1 ; i++){
-            nextPos = current + tank;
-            if(stops[i+1] - stops[i] > tank)
+        int refils = 0;
+        int currentpos = 0;
+        int nextpos=0;
+
+        stops.add(dist);
+        stops.add(0,0);
+        while(stops.get(currentpos)+tank<dist) {
+            currentpos = nextpos;
+            while(nextpos < stops.size() - 1 && stops.get(nextpos + 1)  <= stops.get(currentpos) + tank)
+                nextpos++;
+
+            if(stops.get(currentpos) + tank < dist)
+                refils++;
+            if(nextpos == currentpos)
                 return -1;
-            if(nextPos > stops[i] && nextPos >= stops[i+1] && i!=stops.length-2)
-                continue;
-            if(nextPos >= stops[i]) {
-                locations.add(stops[i]);
-                current += stops[i];
-            }
         }
-        if(locations.get(locations.size()-1) + tank >= dist)
-            return locations.size();
-        else
-            return -1;
+
+        return refils;
     }
 
     public static void main(String[] args) {
@@ -31,9 +29,10 @@ public class CarFueling {
         int dist = scanner.nextInt();
         int tank = scanner.nextInt();
         int n = scanner.nextInt();
-        int[] stops = new int[n];
+        //int[] stops = new int[n];
+        ArrayList<Integer> stops = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            stops[i] = scanner.nextInt();
+            stops.add(scanner.nextInt());
         }
 
         System.out.println(computeMinRefills(dist, tank, stops));
