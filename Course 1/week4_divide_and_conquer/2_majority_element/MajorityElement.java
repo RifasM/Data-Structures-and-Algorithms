0896d23bfb93d4d561f03b06e73c1bde8f9d62e2
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-24 10:40:06 +0530

    Majority Element

---
+++ Course 1/week4_divide_and_conquer/2_majority_element/MajorityElement.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+public class MajorityElement {
+    private static int getMajorityElement(int[] a, int left, int right) {
+        if (left == right) {
+            return -1;
+        }
+        if (left + 1 == right) {
+            return a[left];
+        }
+        HashMap<Integer, Integer> arr = new HashMap<>();
+        for(int i : a){
+            if(arr.containsKey(i)) {
+                Integer val = arr.get(i);
+                arr.put(i, ++val);
+            }
+            else
+                arr.put(i, 1);
+        }
+        int val = (int) arr.keySet().toArray()[0];
+        if( val >= a.length/2)
+            return val;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        if (getMajorityElement(a, 0, a.length) != -1) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
+
