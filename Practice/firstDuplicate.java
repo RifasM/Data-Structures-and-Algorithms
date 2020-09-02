ca2e713ed118e00582f9799969e4f12871fde3b5
Mohammed Rifas S <rifas2475@outlook.com>
2020-09-02 16:36:14 +0530

    firstDuplicate Problem
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- Practice/firstDuplicate.java
+++ Practice/firstDuplicate.java
@@ -18,6 +18,12 @@ public class firstDuplicate {
     }
 
     private static int calcFirstDuplicate(int[] arr, int n) {
+        /*
+          Traverse the array once
+          Convert the value of index of arr[i] to negative of that value
+          So when we arrive at another instance (duplicate) and check the element at that index, it will be -ve
+          Thus, that is the first duplicate
+         */
         for (int i = 0; i < n; i++) {
             if (arr[Math.abs(arr[i]) - 1] < 0)
                 return Math.abs(arr[i]);
