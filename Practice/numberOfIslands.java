d7b91e2ace6971e89907517aebdfac1a53c898ce
Mohammed Rifas S <rifas2475@outlook.com>
2020-09-03 18:33:42 +0530

    number of islands
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- Practice/numberOfIslands.java
+++ Practice/numberOfIslands.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 /*
-Given a 2d grid of 1's and 0's count the no. of island.
+Given a 2d grid of 1's (land) and 0's (water) count the no. of island.
 An island is surrounded by water and is formed by connecting
 adjacent lands horizontally or vertically.
 
@@ -13,5 +15,41 @@ Input:
 
 Output: 1
  */
+/*
+Breath first search
+ */
 public class numberOfIslands {
+    public static void callBFS(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0)
+            return;
+        grid[i][i] = '0';
+        callBFS(grid, i + 1, j);    // up
+        callBFS(grid, i - 1, j);    // down
+        callBFS(grid, i, j - 1);    // left
+        callBFS(grid, i, j + 1);    //right
+    }
+
+    public static int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    callBFS(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        char[][] arr = new char[n][m];
+        for (int i = 0; i < m; i++) {
+            arr[i] = sc.next().toCharArray();
+        }
+        System.out.println(numIslands(arr));
+    }
 }
