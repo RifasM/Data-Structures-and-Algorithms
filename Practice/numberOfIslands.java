7d363219c54c8182f8a0f3b7c16b8e2d48037d69
Mohammed Rifas S <rifas2475@outlook.com>
2020-09-03 17:17:19 +0530

    number of islands
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

---
+++ Practice/numberOfIslands.java
@@ -0,0 +1,17 @@
+/*
+Given a 2d grid of 1's and 0's count the no. of island.
+An island is surrounded by water and is formed by connecting
+adjacent lands horizontally or vertically.
+
+You may assume the four edges of the grid are surrounded by water.
+
+Input:
+11110
+11010
+11000
+00000
+
+Output: 1
+ */
+public class numberOfIslands {
+}
